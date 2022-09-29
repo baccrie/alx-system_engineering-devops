e2a73a5852e4a282dc303edae760bb7a01c74367
baccrie <bakarerilwan1@gmail.com>
2022-09-29 22:44:10 +0100

    Task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,8 @@
-#!/usr/bin/pup
+#!/usr/bin/env pup
 # This file kill the process "killmenow"
 exec { 'pkill':
   command  => 'pkill killmenow',
+  provider => 'shell',
+  user     => 'root',
+  path     => '/usr/local/bin:/usr/bin/:/usr/sbin/:/bin/'
 }
