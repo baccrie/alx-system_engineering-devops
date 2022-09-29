0c2ee78f57eae1149768774db7f1bf6b7803ac8b
baccrie <bakarerilwan1@gmail.com>
2022-09-29 21:58:15 +0100

    Task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 #!/usr/bin/env pup
 # A puppet script that Creates a file
-file { 'school':
-	ensure => file,
-	path => '/tmp/school',
-	mode => '0744',
-	owner => 'www-data',
-	group => 'www-data',
-	content => 'I Love Puppet',
+file { 'Creates a file named school':
+	ensure => file, 
+	path => '/tmp/school', 
+	mode => '0744', 
+	owner => 'www-data', 
+	group => 'www-data', 
+	content => 'I Love Puppet'
 	}
