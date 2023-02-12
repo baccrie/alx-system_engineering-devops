ec71820e7cab28c02788641ac35bebed90c37654
baccrie <bakarerilwan1@gmail.com>
2023-02-12 16:35:36 +0100

    Task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,21 +8,22 @@ import requests
 from sys import argv
 
 
-user_id = int(argv[1])
-users = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                     .format(user_id)).json()
-all_tasks = requests.get(
-        'https://jsonplaceholder.typicode.com/todos').json()
-all_tasks_for_userid = [key for key in all_tasks
-                        if key["userId"] == user_id]
-task_completed = [key for key in all_tasks_for_userid
-                  if key["completed"] is True]
+if __name__ == '__main__':
+    user_id = int(argv[1])
+    users = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                         .format(user_id)).json()
+    all_tasks = requests.get(
+            'https://jsonplaceholder.typicode.com/todos').json()
+    all_tasks_for_userid = [key for key in all_tasks
+                            if key["userId"] == user_id]
+    task_completed = [key for key in all_tasks_for_userid
+                      if key["completed"] is True]
 
-user = users["name"]
-all_tasks = len(all_tasks_for_userid)
-completed = len(task_completed)
+    user = users["name"]
+    all_tasks = len(all_tasks_for_userid)
+    completed = len(task_completed)
 
-print("Employee {} is done with tasks({}/{}):".
-      format(user, completed, all_tasks))
-for key in task_completed:
-    print('\t {}'.format(key['title']))
+    print("Employee {} is done with tasks({}/{}):".
+          format(user, completed, all_tasks))
+    for key in task_completed:
+        print('\t {}'.format(key['title']))
