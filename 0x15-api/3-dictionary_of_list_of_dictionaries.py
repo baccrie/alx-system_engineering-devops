f5bb5de0a550b5affa222a918c5e0d4295d77599
baccrie <bakarerilwan1@gmail.com>
2023-02-12 19:03:07 +0100

    Task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python3
-""" Script that uses JSONPlaceholder API to get information about employee """
+"""
+Playing with a SOAP api sounds exquisite
+"""
+
 import json
 import requests
-import sys
+from sys import argv
 
 
-if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/'
-    user = '{}users'.format(url)
-    res = requests.get(user)
-    json_o = res.json()
-    d_task = {}
-    for user in json_o:
-        name = user.get('username')
-        userid = user.get('id')
-        todos = '{}todos?userId={}'.format(url, userid)
-        res = requests.get(todos)
-        tasks = res.json()
-        l_task = []
-        for task in tasks:
-            dict_task = {"username": name,
-                         "task": task.get('title'),
-                         "completed": task.get('completed')}
-            l_task.append(dict_task)
+if __name__ == '__main__':
+    users = requests.get('https://jsonplaceholder.typicode.com/users').json()
+    all_tasks = requests.get(
+            'https://jsonplaceholder.typicode.com/todos').json()
+    nos = [key['id'] for key in users]
+    names = [key['username'] for key in users]
+    dic = {}
+    for i in nos:
+        all_tasks_for_userid = [key for key in all_tasks if key["userId"] == i]
 
-        d_task[str(userid)] = l_task
-    filename = 'todo_all_employees.json'
-    with open(filename, mode='w') as f:
-        json.dump(d_task, f)
+        for keys in all_tasks_for_userid:
+            keys['task'] = keys['title']
+            keys['username'] = names[i - 1]
+            del keys['title']
+            del keys['id']
+        dic[str(i)] = all_tasks_for_userid
+    for i, j in dic.items():
+        for k in j:
+            del k['userId']
+
+    with open('todo_all_employees.json', 'w') as json_file:
+        json.dump(dic, json_file)
